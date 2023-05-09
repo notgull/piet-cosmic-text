652e4ad449458ea7fe2f0599f945a7c1390f24b7
jtnunley <jtnunley01@gmail.com>
2023-05-09 08:52:35 -0700

    Allow macintosh encoding in font parsing code

---
+++ tests/font_loading.rs
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later OR MPL-2.0
+// This file is a part of `piet-cosmic-text`.
+//
+// `piet-cosmic-text` is free software: you can redistribute it and/or modify it under the
+// terms of either:
+//
+// * GNU Lesser General Public License as published by the Free Software Foundation, either
+//   version 3 of the License, or (at your option) any later version.
+// * Mozilla Public License as published by the Mozilla Foundation, version 2.
+// * The Patron License (https://github.com/notgull/piet-cosmic-text/blob/main/LICENSE-PATRON.md)
+//   for sponsors and contributors, who can ignore the copyleft provisions of the above licenses
+//   for this project.
+//
+// `piet-cosmic-text` is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU Lesser General Public License or the Mozilla Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License and the Mozilla
+// Public License along with `piet-cosmic-text`. If not, see <https://www.gnu.org/licenses/>.
+
+use piet::Text as _;
+use piet_cosmic_text::Text;
+
+const TUFFY: &[u8] = include_bytes!("../fonts/Tuffy.ttf");
+
+#[test]
+fn load_font() {
+    let mut text = Text::new();
+    let name = text.load_font(TUFFY).unwrap();
+    assert_eq!(name.name(), "Tuffy");
+}
