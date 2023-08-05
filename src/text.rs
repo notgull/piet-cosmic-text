8b749c5ee0749c223c55974d4e3ec7bc643ce851
John Nunley <dev@notgull.net>
2023-08-04 17:16:32 -0700

    License change
    Signed-off-by: John Nunley <dev@notgull.net>

--- src/text.rs
+++ src/text.rs
@@ -7,7 +7,6 @@
 // * GNU Lesser General Public License as published by the Free Software Foundation, either
 //   version 3 of the License, or (at your option) any later version.
 // * Mozilla Public License as published by the Mozilla Foundation, version 2.
-
 //
 // `piet-cosmic-text` is distributed in the hope that it will be useful, but WITHOUT ANY
 // WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
